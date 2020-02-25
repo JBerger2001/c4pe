52fc36c188b064391715d421729f7bd222cddd33
bs <56117258+0x1452@users.noreply.github.com>
2020-02-25 15:07:05 +0100

    Add Service that handles authentification (login/register)

---
+++ Feedback API/Feedback API/Services/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Feedback_API.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Feedback_API.Services
+{
+    public interface IAuthService
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
