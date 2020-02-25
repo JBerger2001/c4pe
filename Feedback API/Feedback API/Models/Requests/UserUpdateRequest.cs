a59f0fa896d4f2206294193d6551be660bfa306a
bs <56117258+0x1452@users.noreply.github.com>
2020-02-25 15:09:32 +0100

    Adjust User DTOs for updates and responses to new model structure

--- Feedback API/Feedback API/Models/Requests/UserUpdateRequest.cs
+++ Feedback API/Feedback API/Models/Requests/UserUpdateRequest.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Feedback_API.Models.Responses
+namespace Feedback_API.Models.Domain
 {
-    public class UserResponse
+    public class UserUpdateRequest
     {
-        public long ID { get; set; }
-        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Description { get; set; }
