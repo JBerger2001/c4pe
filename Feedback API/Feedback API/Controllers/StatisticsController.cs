6fcb41493c65487cfb7d60cdb282b50dae24a42b
0x1452 <56117258+0x1452@users.noreply.github.com>
2020-05-01 19:26:29 +0200

    Add /stats endpoint for statistics (User count, Place count, Review count)

---
+++ Feedback API/Feedback API/Controllers/StatisticsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Feedback_API.Models;
+using Feedback_API.Models.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feedback_API.Controllers
+{
+    [Route("api/stats")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly FeedbackContext _context;
+
+        public StatisticsController(FeedbackContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<StatisticsResponse> GetStatistics()
+        {
+            var stats = new StatisticsResponse()
+            {
+                AmountPlaces = _context.Places.Count(),
+                AmountReviews = _context.Reviews.Count(),
+                AmountUsers = _context.Users.Count()
+            };
+
+            return stats;
+        }
+    }
+}
---
+++ Feedback API/Feedback API/Models/Responses/StatisticsResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Feedback_API.Models.Responses
+{
+    public class StatisticsResponse
+    {
+        public int AmountPlaces { get; set; }
+        public int AmountUsers { get; set; }
+        public int AmountReviews { get; set; }
+    }
+}
