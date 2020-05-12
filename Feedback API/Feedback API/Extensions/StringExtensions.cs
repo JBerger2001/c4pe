ac27c26401f58f233c042c83888673069762ce25
0x1452 <56117258+0x1452@users.noreply.github.com>
2020-05-12 13:16:21 +0200

    Add StringExtensions, Contains method that takes a StringComparison parameter

---
+++ Feedback API/Feedback API/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Feedback_API.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool Contains(this string source, string value, StringComparison comparison)
+            => source?.IndexOf(value, comparison) >= 0;
+    }
+}
